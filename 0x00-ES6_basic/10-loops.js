29d8818e1f1b0011b9b6910beedf4a9c6e3ce49a
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-07-26 17:43:38 +0000

    task 10-loop.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
-export default function appendToEachArrayValue(array, appendString) {
-  let myArray = [];	
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue (array, appendString) {
+  const myArray = [];
   for (const arr of array) {
-    myArray.push(appendString + arr)
+    myArray.push(appendString + arr);
   }
 
   return myArray;
